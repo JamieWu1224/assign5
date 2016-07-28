52e30170d9f3740baa94393eb4e06b93ca9e9ceb
JamieWu1224 <jamiewade1224@gmail.com>
2016-07-28 14:43:05 +0800

    Update assign3.py

--- assign3.py
+++ assign3.py
@@ -1,22 +1,13 @@
-with open('test.txt', 'r') as file:
-    lines = [x.replace('\n', '') for x in file.readlines()]
-    print "lines: ", lines
+def namescore(name_string):
+    lines = name_string.split('\n')
     lines.sort()
-    print "lines sorted: ", lines
-
     sums = [sum([ord(letter.lower()) - 96 for letter in line]) for line in lines]
-    print "sums: ", sums
-
     products = [x * (sums.index(x) + 1) for x in sums]
-    print "products: ", products
-
     total = sum(products)
-    print "total: ", total
+    return total
     
     
     
     
-   test.txt
-   CDEF
-   ABC
-   FIJK
+    
+   
